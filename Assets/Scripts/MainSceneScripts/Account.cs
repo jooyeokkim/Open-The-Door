6085f4c3a790a63175f761f052e82a77c5c6f118
jooyeokkim <jooyeok42@naver.com>
2021-07-18 23:28:22 +0900

    20210718

--- Assets/Scripts/MainSceneScripts/Account.cs
+++ Assets/Scripts/MainSceneScripts/Account.cs
@@ -1,16 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+using System.Net.Sockets;
 
 public class Account : MonoBehaviour {
+	public InputField IDInput;
+	public InputField PassInput;
+	public InputField ConfirmPassInput;
+	public Button createAccount;
+	public Button messagePanel;
+	public Text messagePanelText;
+	string LoginUrl;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start(){
+		LoginUrl = "http://"+IP.creatorIP+"/otd/create_account.php";
+		Debug.Log (LoginUrl);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void LoginBtn(){
+		if (IDInput.text.Length == 0 || PassInput.text.Length == 0) {
+			messagePanelText.text = "Enter ID and Password!";
+			messagePanelText.fontSize = 13;
+			messagePanel.gameObject.SetActive (true);
+		}
+		else if (PassInput.text != ConfirmPassInput.text) {
+			messagePanelText.text = "Password doesn't match confirmation!";
+			messagePanelText.fontSize = 12;
+			messagePanel.gameObject.SetActive (true);
+		} else {
+			StartCoroutine (LoginGo ());
+			createAccount.interactable = false;
+		}
+	}	
+
+	IEnumerator LoginGo(){
+		Debug.Log (IDInput.text);
+		Debug.Log (PassInput.text);
+		WWWForm form = new WWWForm ();
+		form.AddField ("Input_user", IDInput.text);
+		form.AddField ("Input_pass", PassInput.text);
+
+		WWW webRequest = new WWW (LoginUrl, form);
+		yield return  webRequest;
+
+		string output = webRequest.text;
+		Debug.Log (webRequest.text);
+		if (output == "duplication") {
+			messagePanelText.text = "This ID already exists!";
+			messagePanelText.fontSize = 13;
+		} else if (output == "success") {
+			messagePanelText.text = "Success!";
+			messagePanelText.fontSize = 14;
+		} else {
+			messagePanelText.text = "Game server is not running now!";
+			messagePanelText.fontSize = 12;
+		}
+		messagePanel.gameObject.SetActive (true);
+		yield return null;
 	}
 }
---
+++ Assets/Scripts/MainSceneScripts/IP.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class IP
+{
+	public static string creatorIP = "121.153.146.101";
+}
